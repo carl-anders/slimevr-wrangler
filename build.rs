5228550e58d4806c33cd50b66d387a65ca39493e
Carl Andersson <carl@carlandersson.net>
2023-01-07 15:25:52 +0100

    Set correct meta names in win executables

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@
 fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("assets/icon.ico");
+    res.set("ProductName", "SlimeVR Wrangler");
+    res.set("FileDescription", "SlimeVR Wrangler");
     res.compile().unwrap();
 }
 
