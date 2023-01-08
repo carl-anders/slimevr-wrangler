f0f9d0e971708265dfa2a22a0c9b7f7c18229fa4
ImUrX <urielfontan2002@gmail.com>
2023-01-08 18:19:43 -0300

    start making listeners

--- src/joycon/linux_integration.rs
+++ src/joycon/linux_integration.rs
@@ -1,11 +1,11 @@
 use std::{collections::HashSet, sync::mpsc, time::Duration};
 use tokio::time::{interval, sleep};
 
-use evdev::enumerate;
+use evdev::{enumerate, Device, EventStream};
 
 use crate::settings;
 
-use super::ChannelData;
+use super::{ChannelData, JoyconDesignType};
 
 const USB_VENDOR_ID_NINTENDO: u16 = 0x057e;
 const USB_DEVICE_ID_NINTENDO_WIIMOTE: u16 = 0x0306;
@@ -15,9 +15,36 @@ const USB_DEVICE_ID_NINTENDO_JOYCONR: u16 = 0x2007;
 const USB_DEVICE_ID_NINTENDO_PROCON: u16 = 0x2009;
 const USB_DEVICE_ID_NINTENDO_CHRGGRIP: u16 = 0x200E;
 
-#[tokio::main(worker_threads = 2)]
+
+fn convert_design(product_code: u16) -> JoyconDesignType {
+    match product_code {
+        USB_DEVICE_ID_NINTENDO_JOYCONL | USB_DEVICE_ID_NINTENDO_PROCON => JoyconDesignType::Left,
+        USB_DEVICE_ID_NINTENDO_JOYCONR => JoyconDesignType::Right,
+        _ => unreachable!(),
+    }
+}
+
+async fn joycon_listener(tx: mpsc::Sender<ChannelData>, settings: settings::Handler, mut input: EventStream) {
+    let mac = input.device().unique_name().unwrap(); // Joycons always have unique name
+    let device_type = convert_design(input.device().input_id().product());
+    loop {
+        let ev = input.next_event().await.unwrap();
+        
+    }
+}
+
+async fn imu_listener(tx: mpsc::Sender<ChannelData>, settings: settings::Handler, mut input: EventStream) {
+    let mac = input.device().unique_name().unwrap(); // Joycons always have unique name
+
+    loop {
+        let ev = input.next_event().await.unwrap();
+        
+    }
+}
+
+#[tokio::main]
 pub async fn spawn_thread(tx: mpsc::Sender<ChannelData>, settings: settings::Handler) {
-    let mut slow_stream = interval(Duration::from_secs(3));
+    let mut slow_stream = interval(Duration::from_secs(5));
     let mut paths = HashSet::new();
     loop {
         slow_stream.tick().await;
@@ -25,6 +52,7 @@ pub async fn spawn_thread(tx: mpsc::Sender<ChannelData>, settings: settings::Han
             if device.input_id().vendor() != USB_VENDOR_ID_NINTENDO
                 || device.input_id().product() != USB_DEVICE_ID_NINTENDO_JOYCONL
                 || device.input_id().product() != USB_DEVICE_ID_NINTENDO_JOYCONR
+                || device.input_id().product() != USB_DEVICE_ID_NINTENDO_PROCON
                 || paths.contains(&path)
             {
                 continue;
@@ -36,11 +64,15 @@ pub async fn spawn_thread(tx: mpsc::Sender<ChannelData>, settings: settings::Han
                 );
                 continue;
             }
-
             paths.insert(path);
+            let tx = tx.clone();
+            let settings = settings.clone();
             // The device name is defined on all nintendo devices in the kernel,
             // so unwrap shouldn't fail...
-            if device.name().unwrap().ends_with("IMU") {}
+            if device.name().unwrap().ends_with("IMU") {
+                let stream = device.into_event_stream().unwrap();
+                tokio::spawn(imu_listener(tx, settings, stream));
+            }
         }
     }
 }
